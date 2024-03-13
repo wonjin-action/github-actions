b1df8adc2125818e2a69c52c5eb3485b4df17ae8
mori.yudai.la <mori.yudai.la@mynavi.jp>
2024-03-13 01:20:07 +0000

    chore: uncomment oidc stack

--- bin/hinagiku-app.ts
+++ bin/hinagiku-app.ts
@@ -92,10 +92,10 @@ const waf = new WafStack(app, `${pjPrefix}-Waf`, {
   pjPrefix: pjPrefix,
 });
 
-// new OidcStack(app, `${pjPrefix}-Oidc`, {
-//   organizationName: config.OidcParam.OrganizationName,
-//   repositoryNames: config.OidcParam.RepositoryNames,
-// });
+new OidcStack(app, `${pjPrefix}-Oidc`, {
+  organizationName: config.OidcParam.OrganizationName,
+  repositoryNames: config.OidcParam.RepositoryNames,
+});
 
 const webApp = new EcsAppStack(app, `${pjPrefix}-Ecs`, {
   vpc: shareResources.vpc,
