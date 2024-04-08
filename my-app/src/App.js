760ace2062e38ba936f4bd32a859e93688ed4f67
ParkWonjin-mj <132994257+ParkWonjin-mj@users.noreply.github.com>
2024-04-08 18:05:18 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd5
+          Learn GithubAction cicd7
         </a>
       </header>
     </div>
